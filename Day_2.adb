ffe040189cdbac27552d0683ac9a541587ab337b
Emmanuel Morfiadakis <xtrememanos@windowslive.com>
2015-12-15 22:54:33 +1100

    What is summary used for???
    Made changes to printing statements in day 2

--- Day_2.adb
+++ Day_2.adb
@@ -4,22 +4,38 @@ with GNAT.String_Split;   use GNAT;
 procedure Day_2 is
 
    Input, Output     : File_Type;
-   Square_Feet       : Integer    := 0;
+   Wrap, Ribbon      : Integer    := 0;
 
-   function GetWrap (dim1 : Integer; dim2 : Integer; dim3: Integer) return Integer is
-      area1    : Integer := dim1*dim2;
-      area2    : Integer := dim2*dim3;
-      area3    : Integer := dim3*dim1;
+
+   function GetWrap (dim1 : Integer; dim2 : Integer; dim3 : Integer) return Integer is
+      area1    : Integer := dim1 * dim2;
+      area2    : Integer := dim2 * dim3;
+      area3    : Integer := dim3 * dim1;
       min_side : Integer := area1;
+
    begin
       if area2 <= area1  and then area2 <= area3 then
          min_side := area2;
       elsif area3 <= area1  and then area3 <= area2 then
          min_side := area3;
       end if;
+
       return 2 * (area1+ area2 +area3) + min_side;
    end GetWrap;
 
+   function GetRibbon(dim1 : Integer; dim2 : Integer; dim3 : Integer) return Integer is
+      per1     : Integer  := 2*(dim1 + dim2);
+      per2     : Integer  := 2*(dim3 + dim2);
+      per3     : Integer  := 2*(dim1 + dim3);
+      min_per  : Integer  := per1;
+   begin
+      if per2 <= per1  and then per2 <= per3 then
+         min_per := per2;
+      elsif per3 <= per1  and then per3 <= per2 then
+         min_per := per3;
+      end if;
+      return dim1 * dim2 * dim3 + min_per;
+   end GetRibbon;
 
 begin
    Open (File => Input,
@@ -32,9 +48,13 @@ begin
          Separator  : String := "x";
       begin
          String_Split.Create(SubStrings, Cur_Line, Separator, String_Split.Multiple);
-         Square_Feet := Square_Feet + GetWrap(Integer'Value(String_Split.Slice (SubStrings, 1)), Integer'Value(String_Split.Slice (SubStrings, 2)), Integer'Value(String_Split.Slice (SubStrings, 3)));
-         Put_Line(Integer'Image(Square_Feet));
+         Wrap := Wrap + GetWrap(Integer'Value(String_Split.Slice (SubStrings, 1)), Integer'Value(String_Split.Slice (SubStrings, 2)), Integer'Value(String_Split.Slice (SubStrings, 3)));
+
+         Ribbon := Ribbon + GetRibbon(Integer'Value(String_Split.Slice (SubStrings, 1)), Integer'Value(String_Split.Slice (SubStrings, 2)), Integer'Value(String_Split.Slice (SubStrings, 3)));
+
       end;
    end loop;
 
+   Put_Line("Wrap amount is" & Integer'Image(Wrap));
+   Put_Line("Ribbon amount is" & Integer'Image(Ribbon));
 end Day_2;
